5ca0f377a17205718d8327ee940225c15defa4d0
Rafael <rafael.noboru@gmail.com>
2024-06-06 08:49:55 -0300

    Quinto Commit

--- src/test/java/fiap/com/br/Ocean/Clean/AI/OceanCleanAiApplicationTests.java
+++ src/test/java/fiap/com/br/Ocean/Clean/AI/OceanCleanAiApplicationTests.java
@@ -1,13 +1,27 @@
 package fiap.com.br.Ocean.Clean.AI;
 
+import java.util.Collections;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OceanCleanAiApplicationTests {
+public class OceanCleanAiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(OceanCleanAiApplication.class);
+        // Define a porta usando a variável de ambiente 'PORT', padrão para 8080 se não estiver definida
+        app.setDefaultProperties(Collections.singletonMap("server.port", getPort()));
+        app.run(args);
+    }
+
+    private static String getPort() {
+        String port = System.getenv("PORT");
+        if (port == null || port.isEmpty()) {
+            port = "8080";  // Porta padrão
+        }
+        return port;
+    }
 
 }
